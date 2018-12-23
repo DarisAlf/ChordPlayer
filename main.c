e6dba6fcdd9bb83393127fd1e374c35281867e93
DarisAlf <44505589+DarisAlf@users.noreply.github.com>
2018-12-23 12:56:04 +0700

    Added updated code

--- main.c
+++ main.c
@@ -2,118 +2,239 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <string.h>
+#include <unistd.h>
 
-//Sebelum di compile, Install MinGW dan compiler di codeblocks harus di reset. Di Linker settings tambahkan library "winmm"
-//mengasign array link ke project file dalam windows.h
+//To compile this program on your own, install the MinGW compiler and reset your settings in CodeBlocks to default.
+//Add "winmm" in the "linker" tab. This is necessary to run the function "playsound"
+
 
 LPCTSTR chords[15] = {"No Sound" ,"C Major", "D Minor", "E Minor", "F Major", "G Major", "A Minor", "E Major",
                       "C Major Half", "D Minor Half", "E Minor Half", "F Major Half", "G Major Half", "A Minor Half", "E Major Half"};
 
+struct node
+{
+    int chords;
+    struct node *next;
+};
+
+struct node *head = NULL;
+FILE *chordFile;
+
 void playChord(int i);
 void printChord(int i);
-void printDisplay(int array[], int limit);
+void printDisplay();
+void insert(int chords);
+void deleteChord();
+void Reverse(struct node* head);
+void playProgression(struct node *head);
+int chooseFile(int choice);
+void readFile();
+void writeFile();
+void deleteAllChord();
 int getInteger();
 
 int main()
 {
-    int bar = 0;
-    int progression [32] = {0};
-    int halfCounter = 0;
+
     int errorchecker;
 
     printf("=========================================\n");
-    printf("Welcome to the ChordPlayer Program   !!!!\n");
+    printf("Welcome to ChordPlayer !!!\n");
     printf("=========================================\n");
-    printf("\nPress Enter to Continue...");
+    printf("\nPress enter to continue...");
     getchar();
     fflush(stdin);
-    system("cls");
-    printf("===================================================\n");
-    printf("                   ChordPlayer\n");
-    printf("===================================================\n\n");
-    printf("Input the number of bars that you want to play \n\nThe number of bars must be a multiply of 4 and less than 32\n\n");
-    printf("Number of Bars: ");
 
-    while (bar = getInteger())
+    while (1)
     {
-        if (bar >= 0 && bar <= 32 && bar % 4 == 0)
-            break;
-        else
-            printf("Input is invalid, please re-enter : ");
-    }
+        int choice = 0;
+        int stop = 0;
+        int halfCounter = 0;
 
-    printDisplay(progression, bar);
+        deleteAllChord();
+        system("cls");
+        printf("===================================================\n");
+        printf("                   ChordPlayer\n");
+        printf("===================================================\n\n");
+        printf("Menu\n");
+        printf("1. Create file\n");
+        printf("2. Play file\n");
+        printf("3. Exit\n");
+        printf("Your choice: ");
 
-    for (int j = 0; j < bar; ++j)
-    {
-        int chordNumber;
-
-        printf("Input Code:\n [1] C Major [8] 1/2 C Major\n [2] D Minor [9] 1/2 D Minor\n [3] E Minor [10] 1/2 E Minor\n [4] F Major [11] 1/2 F Major\n [5] G Major [12] 1/2 G Major\n [6] A Minor [13] 1/2 A Minor\n [7] E Major [14] 1/2 E Major\n [0] No Sound\n\n");
-        printf("Chord %d : ", j + 1);
-
-        fflush(stdin);
-        if (halfCounter % 2 != 0)
+        while (choice = getInteger())
         {
-            while(chordNumber = getInteger())
-            {
-                if (chordNumber >= 0 && chordNumber <= 14)
-                    break;
-                else
-                    printf("Input is invalid, please re-enter : ");
+            if (choice == 1) {
+                printf("Enter the file's name: ");
+                chooseFile(choice);
+                break;
             }
-        }
-        else
-        {
-            while(chordNumber = getInteger())
-            {
-                if (chordNumber >= 0 && chordNumber <= 14)
-                    break;
-                else
-                    printf("Input is invalid, please re-enter : ");
+            else if (choice == 2) {
+                printf("Enter the file's name: ");
+                if (chooseFile(choice) != 1) {
+                    printf("File does not exist!");
+                    getchar();
+                    fflush(stdin);
+                };
+                break;
+            }
+            else if (choice == 3) {
+                return 0;
+            }
+            else {
+                printf("ERROR");
             }
         }
 
-        progression[j] = chordNumber;
+        printDisplay(choice);
 
-        if (chordNumber > 7 && chordNumber < 15)
+        int chordCounter = 0;
+        if (choice == 1)
         {
-            halfCounter += 1;
+            while (stop == 0)
+            {
+                int chordNumber;
+
+                printf("Input Code:\n [1] C Major [8] 1/2 C Major\n [2] D Minor [9] 1/2 D Minor\n [3] E Minor [10] 1/2 E Minor\n [4] F Major [11] 1/2 F Major\n [5] G Major [12] 1/2 G Major\n [6] A Minor [13] 1/2 A Minor\n [7] E Major [14] 1/2 E Major\n [15] Delete [16] Finish    \n\n");
+                printf("Chord %d : ", chordCounter + 1);
+
+                fflush(stdin);
+
+
+                while(chordNumber = getInteger())
+                {
+                    if (halfCounter % 2 != 0)
+                    {
+                        if (chordNumber >= 8 && chordNumber <= 15)
+                            break;
+                        else if (chordNumber == 16)
+                            printf("Please add a half chord to finish: ");
+                        else
+                            printf("Enter between 8 to 14: ");
+                    }
+                    else
+                    {
+                        if (chordNumber >= 1 && chordNumber <= 16)
+                        break;
+                    else
+                        printf("Enter between 1 to 14: ");
+                    }
+                }
+
+                if (chordNumber > 7 && chordNumber < 15)
+                {
+                    halfCounter += 1;
+                }
+
+                if (chordNumber >= 1 && chordNumber <= 14) {
+                    insert(chordNumber);
+                    chordCounter++;
+                }
+
+                else if (chordNumber == 15) {
+                    if (head->chords > 7 && head->chords < 15)
+                        halfCounter -= 1;
+                        deleteChord();
+                    if (chordCounter >= 1) {
+                        printf("Nothing to delete!");
+                        chordCounter--;
+                    }
+                }
+
+                else if (chordNumber == 16)
+                    stop = 1;
+
+                printDisplay(choice);
+            }
+            writeFile();
+            fclose(chordFile);
+            printf("Your file has been saved!");
+            }
+
+        else
+        {
+            readFile();
+            printf("Playing: \n");
+            playProgression(head);
+            fclose(chordFile);
         }
-
-        if (halfCounter % 2 != 0)
-            bar++;
-
-        printDisplay(progression, bar);
     }
-
-    printf("Playing: \n");
-
-    for (int i = 0; i < bar; i++)
-    {
-        printChord(progression[i]);
-        playChord(progression[i]);
-    }
-
-    return 0;
 }
 
-void printDisplay(int array[], int limit)
+int chooseFile(int choice) {
+    int sucess = 0;
+    fflush(stdin);
+    char fname[20];
+    char *fn = malloc(strlen(fname+5));
+    fgets(fname, 20, stdin);
+    fname[strcspn(fname, "\n")] = 0;
+    sprintf(fn, "%s.txt", fname);
+    if (choice == 1) {
+        chordFile = fopen(fn, "w");
+        sucess = 1;
+    }
+    else {
+        if (chordFile = fopen(fn, "r")) {
+            sucess = 1;
+        }
+    }
+    free(fn);
+    return sucess;
+}
+
+void readFile() {
+    char buffer[255];
+    int chordNumber;
+    while(fgets(buffer, 255, chordFile)) {
+        buffer[strcspn(buffer, "\n")] = 0;
+        sscanf(buffer, "%d", &chordNumber);
+        insert(chordNumber);
+    }
+}
+
+void writeFile() {
+    fflush(chordFile);
+    struct node *temp = head;
+    while (temp != NULL) {
+        fprintf(chordFile, "%d\n", temp->chords);
+        temp = temp->next;
+    }
+}
+
+void printDisplay(int choice)
 {
+    struct node *temp = head;
+
     system("cls");
     printf("===================================================\n");
     printf("                   ChordPlayer\n");
     printf("===================================================\n\n");
-    printf("Chord Progression:\n");
-
-    for (int k = 0; k < limit; ++k)
-    {
-    if (array[k] != 0)
-            printChord(array[k]);
-        else
-            printf("_ ");
+    if (choice == 1) {
+        printf("Current chords:\n");
+        Reverse(temp);
+        printf("_ ");
     }
     printf("\n\n");
 }
+
+void playProgression(struct node *head)
+{
+    struct node *temp = head;
+    while (temp != NULL) {
+        printChord(temp->chords);
+        playChord(temp->chords);
+        temp = temp->next;
+    }
+}
+
+void Reverse(struct node *head)
+{
+    if (head == NULL)
+       return;
+    Reverse(head->next);
+    printChord(head->chords);
+}
+
 
 void playChord(int i)
 {
@@ -142,6 +263,31 @@ void printChord(int i)
     }
 }
 
+void insert(int chords) {
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+
+   link->chords = chords;
+   link->next = head;
+   head = link;
+}
+
+void deleteChord() {
+    if (head != NULL) {
+        struct node *temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+void deleteAllChord() {
+    struct node *temp = head;
+    while (temp != NULL) {
+        free(temp);
+        temp = temp->next;
+    }
+    head = NULL;
+}
+
 int getInteger()
 {
     char *end, str[20];
@@ -151,9 +297,10 @@ int getInteger()
         number = strtol(str, &end, 10);
         if (end == str || *end != '\n')
         {
-            printf("Input is invalid, please re-enter : ");
+            printf("Invalid input, please re-input: ");
         }
         else
             return number;
     }
 }
+
